b9ef72d02922f3cccda916594e31d7a0344e7568
Can <54573938+Teragron@users.noreply.github.com>
2023-05-06 22:51:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ from document_chatbot import DocumentChatbot
 
 document_chatbot = DocumentChatbot()
 
+
+
+#os.environ["HUGGINGFACEHUB_API_TOKEN"] = "xxxxxxxxxxxxxxx" (uncomment this line and paste your own hf token to run the models)
+
 with gr.Blocks() as demo:
     title = """<p><h1 align="center" style="font-size: 36px;">Talk with your document</h1></p>"""
     gr.HTML(title)
@@ -13,17 +17,12 @@ with gr.Blocks() as demo:
         text_input = gr.Textbox(label="Enter text or URL to text file")
         with gr.Column():
             with gr.Row():
-                api_key_input = gr.Textbox(label="Enter HF Token")
-                picked_model = gr.Dropdown(["google/flan-t5-large", "google/flan-t5-base","google/flan-t5-small"], label="Models", interactive=True)
-                load_configs = gr.Button("Load configs")
-                load_configs.click(document_chatbot.load_token_and_model, inputs=[api_key_input, picked_model], outputs=[api_key_input])
-                
+                picked_model = gr.Dropdown(["google/flan-t5-large", "google/flan-t5-base","google/flan-t5-small"], label="Models", interactive=True)  
+            
             chatbot = gr.Chatbot()
-
             q_input = gr.Textbox(label="Please write your question")
             clear = gr.Button("Clear")
-            q_input.submit(document_chatbot.respond, [text_input, q_input, chatbot], [q_input, chatbot])
+            q_input.submit(document_chatbot.respond, [text_input, q_input, chatbot, picked_model], [q_input, chatbot])
             clear.click(lambda: None, None, chatbot, queue=False)
-        
         
 demo.launch(debug=True)
